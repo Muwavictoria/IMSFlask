88acc392448b6ab1df4866e78f537d972ba63f2c
Muwavictoria <victoriamuwanguzi!9@gmail.com>
2023-10-18 14:15:58 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ims.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///inventory_system_db.db"
 app.config['SECRET_KEY']='secretkey'
 
 db = SQLAlchemy(app)
